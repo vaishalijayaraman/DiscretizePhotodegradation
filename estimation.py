a6944f8206f804d737bd7db2b4b401e2ee03aed8
Vaishali Jayaraman <vaishalijayaraman@wireless-10-145-71-134.public.utexas.edu>
2018-11-28 13:36:54 -0600

    Finished implicit method

--- estimation.py
+++ estimation.py
@@ -45,15 +45,11 @@ t0 = 0
 tf = 1
 t_step = 1/20
 
-alpha_pd_star = alpha_pd*L*c_pd0 # (divide by 3)
-alpha_deg_star = alpha_deg*L*c_pd0
-
-# GRID
 x_num = int((xf-x0) / x_step)
 t_num = int((tf-t0) / t_step)
-grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
-grid.fill(0.0)
-grid[0] = 1.0
+
+alpha_pd_star = alpha_pd*L*c_pd0 
+alpha_deg_star = alpha_deg*L*c_pd0
 
 # FUNCTIONS
 # PRETTY PRINT
@@ -63,6 +59,18 @@ def prettyprint(mat, fmt="g"):
         for i, y in enumerate(x):
             print(("{:"+str(col_maxes[i])+fmt+"}").format(y), end=" ")
         print("")
+
+# INTENSITY CALCULATION
+def computeIntensity(prev, j, alpha_pd_star, alpha_deg_star, x_num):
+	summation = 0
+	for k in range (j, x_num):
+		s = ((alpha_pd_star * prev[k]) - (alpha_deg_star * (1-prev[k])))/(x_num)
+		summation = summation + s
+
+	exponent = (-1 * summation)
+	intensity = m.exp(exponent)
+
+	return intensity
 
 # EXPLICIT METHOD
 def explicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num):
@@ -75,21 +83,50 @@ def explicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star
 
 	return grid
 
-def computeIntensity(prev, j, alpha_pd_star, alpha_deg_star, x_num):
-	summation = 0
-	for k in range (j, x_num):
-		s = ((alpha_pd_star * prev[k]) - (alpha_deg_star * (1-prev[k])))/(x_num)
-		summation = summation + s
+# NEWTON'S METHOD
+def newton(f,Df,x0,epsilon,max_iter):
+	xn = x0
+	for n in range(0,max_iter):
+		fxn = f(xn)
+		if abs(fxn) < epsilon:
+			#print('Found solution after',n,'iterations.')
+			return xn
+		Dfxn = Df(xn)
+		if Dfxn == 0:
+			#print('Zero derivative. No solution found.')
+			return None
+		xn = xn - fxn/Dfxn
+	#print('Exceeded maximum iterations. No solution found.')
+	return None
 
-	exponent = (-1 * summation)
-	intensity = m.exp(exponent)
+# IMPLICIT METHOD
+def implicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num):
+	for i in range (1, t_num): 
+		prev = grid[(i-1)]
+		
+		for j in range(0, x_num):
+			v = computeIntensity(prev, j, alpha_pd_star, alpha_deg_star, x_num)
 
-	return intensity
+			f = lambda x: x - prev[j] + (t_step * v * x)
+
+			Df = lambda x: 1 + (t_step * v) + (-1 * t_step * x * ((alpha_deg_star + alpha_pd_star)/x_num) * v)
+
+			value = newton(f,Df,prev[j],1e-10,10)
+			grid[i][j] = value
+
+	return grid
 
 # MAIN 
-print("Initial Grid: ")
+
+# SET GRID
+grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
+grid.fill(0.0)
+grid[0] = 1.0
+
+print("Initial Grid: ") 
 prettyprint(grid)
 
+print("EXPLICIT: ")
 g = explicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num)
 grid = np.transpose(g)
 
@@ -99,6 +136,25 @@ prettyprint(grid)
 # GRAPH 
 plt.imshow(grid, interpolation='none', cmap=cm.Blues)
 plt.show()
+
+# RESET GRID
+grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
+grid.fill(0.0)
+grid[0] = 1.0
+
+print("IMPLICIT: ")
+g = implicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num)
+grid = np.transpose(g)
+
+print("Resulting Grid: ")
+prettyprint(grid)
+
+# GRAPH 
+plt.imshow(grid, interpolation='none', cmap=cm.Blues)
+plt.show()
+
+
+
 
 
 
