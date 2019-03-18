df5a36414de04b22a90e2fa3c25976a9b5c16d1e
Vaishali Jayaraman <vaishalijayaraman@Vaishalis-MBP.home>
2019-03-18 09:36:09 -0400

    Finished implicit diffusion modelling, mask loading, mesh triangularization support and aBar, bBar calculation

--- diffusionless.py
+++ diffusionless.py
@@ -124,24 +124,24 @@ grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
 grid.fill(0.0)
 grid[0] = 1.0
 
-# print("Initial Grid: ") 
-# prettyprint(grid)
+print("Initial Grid: ") 
+prettyprint(grid)
 
-# print("EXPLICIT: ")
-# g = explicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num)
-# grid = np.transpose(g)
+print("EXPLICIT: ")
+g = explicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num)
+grid = np.transpose(g)
 
-# print("Resulting Grid: ")
-# prettyprint(grid)
+print("Resulting Grid: ")
+prettyprint(grid)
 
-# # GRAPH 
-# plt.imshow(grid, interpolation='none', cmap=cm.Blues)
-# plt.show()
+# GRAPH 
+plt.imshow(grid, interpolation='none', cmap=cm.Blues)
+plt.show()
 
-# # RESET GRID
-# grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
-# grid.fill(0.0)
-# grid[0] = 1.0
+# RESET GRID
+grid = np.arange(x_num*t_num*1.0).reshape((t_num, x_num))
+grid.fill(0.0)
+grid[0] = 1.0
 
 print("IMPLICIT: ")
 g = implicit(grid, t0, tf, t_step, x0, xf, x_step, alpha_pd_star, alpha_deg_star, x_num)
--- mask.py
+++ mask.py
@@ -1,1 +1,13 @@
+import cv2
 
+# LOAD MASK
+img_path = 'img.jpg'
+img = cv2.imread(img_path, 0)
+img_grid = img / 255.0 #sets black to 0 and white to 1 
+
+img_grid = np.transpose(img_grid)
+
+# CREATE GRID
+print("Image is: ", img)
+
+print("Image is: ", img_grid)
